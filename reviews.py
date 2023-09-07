36ca0ec00a3288be1bbabb3f628ed3d6d4dd2c5c
KJSturgill <122930021+KJSturgill@users.noreply.github.com>
2023-09-07 13:45:49 -0400

    commit
    i think this works

--- reviews.py
+++ reviews.py
@@ -1,2 +1,9 @@
-# add your code here
+import pandas as pd
 
+reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip', compression='zip')
+
+wine_by_countries = reviews.groupby('country').agg({'country':'count', 'points': 'mean'}).round(1)
+
+new_reviews = wine_by_countries.rename(columns={'country':'count'}).reset_index()
+
+new_reviews.to_csv('data/reviews-per-country.csv', index=False)
