25599ae2ee8cd59311fc2a81e7189da3d34eac2f
KJSturgill <122930021+KJSturgill@users.noreply.github.com>
2023-09-07 14:11:36 -0400

    commit2
    I fixed it

--- reviews.py
+++ reviews.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import zipfile36
 
-reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip', compression='zip')
+reviews = pd.read_csv('C:/Users/bisho/Desktop/CKY DA/tues-data-analysis/wine-reviews-exercise-KJSturgill/data/winemag-data-130k-v2.csv.zip', compression='zip')
 
 wine_by_countries = reviews.groupby('country').agg({'country':'count', 'points': 'mean'}).round(1)
 
 new_reviews = wine_by_countries.rename(columns={'country':'count'}).reset_index()
 
-new_reviews.to_csv('data/reviews-per-country.csv', index=False)
+new_reviews.to_csv('C:/Users/bisho/Desktop/CKY DA/tues-data-analysis/wine-reviews-exercise-KJSturgill/data/reviews-per-country.csv', index=False)
